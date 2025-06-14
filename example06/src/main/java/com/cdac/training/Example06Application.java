d6456116b398b4562d28248ade3ad858e1a134e7
Majrul Ansari <majrul@gmail.com>
2025-06-14 15:53:09 +0530

    docker demos

---
+++ example06/src/main/java/com/cdac/training/Example06Application.java
@@ -0,0 +1,13 @@
+package com.cdac.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Example06Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Example06Application.class, args);
+	}
+
+}
---
+++ example06/src/test/java/com/cdac/training/Example06ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdac.training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Example06ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
